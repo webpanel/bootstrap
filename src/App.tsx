c9ac5cf40be4bec317923756d149f77708499f22
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2019-08-06 01:30:30 +0200

    Add redirect to first entity

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import 'moment/locale/cs';
 
 import * as React from 'react';
 
-import { Admin } from 'webpanel-admin';
+import { Admin, Layout } from 'webpanel-admin';
+
 import { ENV } from './env';
 import { LocaleProvider } from 'antd';
+import { Redirect } from 'react-router';
 import en_US from 'antd/lib/locale-provider/en_GB';
 import { entities } from './model';
 
@@ -20,6 +22,14 @@ export const App = () => {
           scope: ENV.REACT_APP_SCOPE
         }}
         entities={entities}
+        structureItems={[
+          <Layout.StructureItem
+            key="/"
+            name="redirect-to-first-entity"
+            content={<Redirect to={entities[0].getListLink()} />}
+          />,
+          ...entities.map(x => x.structureItem())
+        ]}
       />
     </LocaleProvider>
   );
